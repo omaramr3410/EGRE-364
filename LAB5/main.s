2ee286839280622edf9bb8e95770a19fde39e4f7
Omar Amr <omaramr3410@gmail.com>
2019-10-17 15:47:48 -0400

    finshed code

--- LAB5/main.s
+++ LAB5/main.s
@@ -66,109 +66,167 @@ __main	PROC
 	STR r1, [r0, #GPIO_PUPDR]
 	
 	LDR r0,=steps ; loads array 
-	LDR r12, =1200 ; base delay var
-	LDR r11, =4000 ; delay max
-	LDR r10, =400  ; delay min
+	LDR r12, =10000 ; base delay var
+	LDR r11, =30000 ; delay max
+	LDR r10, =2000  ; delay min
+	
+	B full
 
-st	LDR r1, [r0],#4
+
+full
 	LDR r2, =GPIOE_BASE
 	LDR r3, [r2, #GPIO_ODR]
-	LDR r4,=0x20000024
-	CMP r0, r4
-	BEQ reset
-	BNE ret3	
-ret3	BIC r3, r3, #(0xF<<12)
-	ORR r3, r3, r1
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x1<<12)
 	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x1<<13)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x1<<14)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+		
+	LDR r2, =GPIOE_BASE
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x1<<15)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	BL checkA12
+	BL checkA35
+	
+	BL full
+
+half
+	LDR r2, =GPIOE_BASE     ; step 1 
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x1<<12)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE     ; step 2
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x3<<12)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE     ; step 3
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x1<<13)  
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE     ; step 4
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x3<<13)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE     ; 
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x1<<14)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x3<<14)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x1<<15)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	LDR r2, =GPIOE_BASE
+	LDR r3, [r2, #GPIO_ODR]
+	BIC r3, r3, #(0xF<<12)
+	ORR r3, r3, #(0x9<<12)
+	STR r3, [r2, #GPIO_ODR]
+	BL delay 
+	
+	BL checkA12
+	BL checkA35
+	
+	BL half
+
+
+
+checkA35	
 	LDR r4, =GPIOA_BASE 
 	LDR r5,[r4,#GPIO_IDR]
 	LDR r6, =0x8
 	AND r7,r6,r5
-	CMP r7,#0x8
+	CMP r7,r6
 	BEQ incspeed
-	BNE return1
-return1	
+	
 	LDR r4, =GPIOA_BASE 
 	LDR r5,[r4,#GPIO_IDR]
 	LDR r6, =0x20
-	AND r7,r6,r4
-	CMP r7,#0x2
+	AND r7,r6,r5
+	CMP r7,r6
 	BEQ decspeed
-	BNE return2
-return2
-	
-	B delay 
-	
-delay 	PUSH{r0,r1,r2,r3,r12}
-		;LDR r12,=1200
-subin	SUB r12, r12, #1
-		LDR r2, =GPIOE_BASE
-		LDR r3, [r2, #GPIO_ODR]	
-		BIC r3, r3, #(0x1<<10)
-		ORR r3,r3,#(0x1<<10)
-		STR r3, [r2, #GPIO_ODR]
-		CMP r12, #0x0
-		BEQ p
-		BNE subin
-p		POP{r0,r1,r2,r3,r12}
-		B st
-	
-reset	LDR r0, =steps
-		B ret3
-		
-A3Check LDR r4, =GPIOA_BASE 
-		 LDR r5,[r4,#GPIO_IDR]
-		 LDR r6, =0x28
-		 AND r7,r6,r4
-		 CMP r7,#0x8
-		 BEQ incspeed
-		 BNE return1
-		 	 
-incspeed	LDR r4, =GPIOA_BASE 
-			LDR r5,[r4,#GPIO_IDR]
-			LDR r6, =0x28
-			AND r7,r6,r4
-			CMP r7,#0x8
-			BEQ subn
-			BNE return1
-			
-subn		CMP r11,r12
-			BLT check2
-			BGE return1
-check2		CMP r12,r10
-			BGE go
-			BLT return2
-go			SUB r12, #500
-			B return1
-			 	
 
-A5Check LDR r4, =GPIOA_BASE 
-		 LDR r5,[r4,#GPIO_IDR]
-		 LDR r6, =0x28
-		 AND r7,r6,r4
-		 CMP r7,#0x2
-		 BEQ decspeed
-		 BNE return2
+checkA12
+	LDR r4, =GPIOA_BASE 
+	LDR r5,[r4,#GPIO_IDR]
+	LDR r6, =0x2
+	
+	AND r7,r6,r5
+	CMP r7,r6
+	BEQ half
+	
+	LDR r4, =GPIOA_BASE 
+	LDR r5,[r4,#GPIO_IDR]
+	LDR r6, =0x4
+	AND r7,r6,r5
+	CMP r7,r6
+	BEQ full
+	
+
+	
+delay  MOV r9,r12
+delaya CMP r9, #0
+	   SUB r9,r9,#1
+	   BNE delaya
+	   BX LR	
+	
+incspeed 
+		SUB r12,#100
+		CMP r12,r10
+		BGE delay
+		MOV r12,r10
+		B delay
 		 
+
+decspeed 
+		ADD r12,#100
+		CMP r12,r11
+		BLE delay
+		MOV r12,r11
+		B delay
 		 
-		 
-decspeed	LDR r4, =GPIOA_BASE 
-			LDR r5,[r4,#GPIO_IDR]
-			LDR r6, =0x28
-			AND r7,r6,r4
-			CMP r7,#0x2
-			BEQ addn
-			BNE st
-			
-addn		
-			CMP r11,r12
-			BGE checkcond
-			BLT return2
-checkcond	CMP r12,r10
-			BGE addr
-			BLT return2	
-addr		ADD r10, #5
-			B return2
+
+
 
 	ENDP
 
