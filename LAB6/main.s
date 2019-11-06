8480bb0903971108fe291f73592e56eb711fda43
Papa Beye <32583389+PapaBeye@users.noreply.github.com>
2019-11-05 19:12:35 -0500

    update

--- LAB6/main.s
+++ LAB6/main.s
@@ -120,232 +120,114 @@ __main	PROC
 	BIC r1, r1, #(0xFF)
 	STR r1, [r0, #GPIO_PUPDR]
 	
-start	
-	LDR r2, =GPIOE_BASE     
+st	LDR r2, =GPIOE_BASE     
 	LDR r3, [r2, #GPIO_ODR]
 	BIC r3, r3, #(0xF<<12)
 	STR r3, [r2, #GPIO_ODR]
-	BL delay 
+	LDR r9, = 500000
+	LDR r4, = (0x1<<12)
+	B delayst
+checkcols	LDR r2, =GPIOA_BASE
+			LDR r5,[r2,#GPIO_IDR]
+			LDR r6, =0xF
+			AND r7,r6,r5
+			CMP r7,#(0x0)
+			BEQ set
+			CMP r7,#(0x1)
+			BEQ col1
+			CMP r7,#(0x2)
+			BEQ col2
+			CMP r7,#(0x4)
+			BEQ col3
+			CMP r7,#(0x8)
+			BEQ col4
+			B st
 	
-checkcols	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xF
-	AND r7,r6,r5
-	CMP r7,#(0xF)
-	BEQ start
-	BL delay
-
-delay  MOV r9,r12
-delaya CMP r9, #0
-	   SUB r9,r9,#1
-	   BNE delaya
-	   BX LR
-test1110
-	LDR r2, =GPIOE_BASE     
+set	LDR r2, =GPIOE_BASE     
 	LDR r3, [r2, #GPIO_ODR]
 	BIC r3, r3, #(0xF<<12)
-	ORR r3, r3, #(0xE<<12)
+	ORR r3, r3, r4
+	MOV r11, r4
+	LSL r4, r4, #1 
 	STR r3, [r2, #GPIO_ODR]
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xE
-	AND r7,r6,r5
-	CMP r7,#(0xE)
-	BEQ cols1  ;pressed button in row 1
-	BNE test1101 
+	CMP r11, #0
+	BEQ st
+	LDR r9, = 500000
+	B delayst
 
-test1101 
-	LDR r2, =GPIOE_BASE     
-	LDR r3, [r2, #GPIO_ODR]
-	BIC r3, r3, #(0xF<<12)
-	ORR r3, r3, #(0xD<<12)
-	STR r3, [r2, #GPIO_ODR]
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xD
-	AND r7,r6,r5
-	CMP r7,#(0xD)
-	BEQ cols2  ;pressed button in row 2
-	BNE test1011 
-
-test1011 
-	LDR r2, =GPIOE_BASE     
-	LDR r3, [r2, #GPIO_ODR]
-	BIC r3, r3, #(0xF<<12)
-	ORR r3, r3, #(0xB<<12)
-	STR r3, [r2, #GPIO_ODR]
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xB
-	AND r7,r6,r5
-	CMP r7,#(0xB)
-	BEQ cols3  ;pressed button in row 3
-	BNE test0111
-
-test0111 
-	LDR r2, =GPIOE_BASE     
-	LDR r3, [r2, #GPIO_ODR]
-	BIC r3, r3, #(0xF<<12)
-	ORR r3, r3, #(0x7<<12)
-	STR r3, [r2, #GPIO_ODR]
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0x7
-	AND r7,r6,r5
-	CMP r7,#(0x7)
-	BEQ cols4  ;pressed button in row 4
-	BNE start ; restart since not read as pressed 
-
-	
-cols1 
-	LDR r2, =GPIOE_BASE     
-	LDR r3, [r2, #GPIO_ODR]
-	BIC r3, r3, #(0xF<<12)
-	STR r3, [r2, #GPIO_ODR]
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xE
-	AND r7,r6,r5
-	CMP r7,#(0xE)
-	BEQ.W pressedA ; button A pressed
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xD
-	AND r7,r6,r5
-	CMP r7,#(0xD)
-	BEQ.W pressed3 ; button 3 pressed
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xB
-	AND r7,r6,r5
-	CMP r7,#(0xB)
-	BEQ.W pressed2 ; button 2 pressed
-
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0x7
-	AND r7,r6,r5
-	CMP r7,#(0x7)
-	BEQ.W pressed1 ; button 1 pressed
+col1 	CMP r11, #(0x1<<12)
+		BEQ pone
+		CMP r11, #(0x1<<13)
+		BEQ pfour
+		CMP r11, #(0x1<<14)
+		BEQ psev
+		CMP r11, #(0x1<<15)
+		BEQ pstar
+col2 	CMP r11, #(0x1<<12)
+		BEQ ptwo
+		CMP r11, #(0x1<<13)
+		BEQ pfive
+		CMP r11, #(0x1<<14)
+		BEQ pate
+		CMP r11, #(0x1<<15)
+		BEQ pzero
+col3 	CMP r11, #(0x1<<12)
+		BEQ pthree
+		CMP r11, #(0x1<<13)
+		BEQ psix
+		CMP r11, #(0x1<<14)
+		BEQ pnine
+		CMP r11, #(0x1<<15)
+		BEQ phash
+col4 	CMP r11, #(0x1<<12)
+		BEQ pA
+		CMP r11, #(0x1<<13)
+		BEQ pB
+		CMP r11, #(0x1<<14)
+		BEQ p_C
+		CMP r11, #(0x1<<15)
+		BEQ pD
 
 
+	
+delayst	SUB r9,r9,#1
+		CMP r9, #0
+		BNE delayst
+		B checkcols
+		
 
-cols2
-	LDR r2, =GPIOE_BASE     
-	LDR r3, [r2, #GPIO_ODR]
-	BIC r3, r3, #(0xF<<12)
-	STR r3, [r2, #GPIO_ODR]
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xE
-	AND r7,r6,r5
-	CMP r7,#(0xE)
-	BEQ pressedB ; button B pressed
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xD
-	AND r7,r6,r5
-	CMP r7,#(0xD)
-	BEQ pressed6 ; button 6 pressed
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xB
-	AND r7,r6,r5
-	CMP r7,#(0xB)
-	BEQ pressed5 ; button 5 pressed
-
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0x7
-	AND r7,r6,r5
-	CMP r7,#(0x7)
-	BEQ pressed4 ; button 4 pressed
-	
-
-
-cols3
-	LDR r2, =GPIOE_BASE     
-	LDR r3, [r2, #GPIO_ODR]
-	BIC r3, r3, #(0xF<<12)
-	STR r3, [r2, #GPIO_ODR]
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xE
-	AND r7,r6,r5
-	CMP r7,#(0xE)
-	BEQ pressedC ; button A pressed
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xD
-	AND r7,r6,r5
-	CMP r7,#(0xD)
-	BEQ pressed9 ; button 9 pressed
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xB
-	AND r7,r6,r5
-	CMP r7,#(0xB)
-	BEQ pressed8 ; button 8 pressed
-
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0x7
-	AND r7,r6,r5
-	CMP r7,#(0x7)
-	BEQ pressed7 ; button 7 pressed
-
-
-
-cols4
-	LDR r2, =GPIOE_BASE     
-	LDR r3, [r2, #GPIO_ODR]
-	BIC r3, r3, #(0xF<<12)
-	STR r3, [r2, #GPIO_ODR]
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xE
-	AND r7,r6,r5
-	CMP r7,#(0xE)
-	BEQ pressedD ; button D pressed
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xD
-	AND r7,r6,r5
-	CMP r7,#(0xD)
-	BEQ pressedhash ; button # pressed
-	
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0xB
-	AND r7,r6,r5
-	CMP r7,#(0xB)
-	BEQ pressed0 ; button 0 pressed
-
-	LDR r4, =GPIOA_BASE 
-	LDR r5,[r4,#GPIO_IDR]
-	LDR r6, =0x7
-	AND r7,r6,r5
-	CMP r7,#(0x7)
-	BEQ pressedstar ; button * pressed
-pressedB
-	LDR r12, =0x42
+pB	LDR r12, =0x42
 	B write
+pA	LDR r12, =0x41
+	B write
+pthree	LDR r12, =0x3
+	B write
+ptwo	LDR r12, =0x2
+	B write
+pone	LDR r12, =0x1
+	B write
+psix	LDR r12, =0x6
+	B write
+pfive	LDR r12, =0x5
+	B write
+pfour	LDR r12, =0x4
+	B write
+p_C	LDR r12, =0x43
+	B write
+pnine	LDR r12, =0x9
+	B write
+pate	LDR r12, =0x8
+	B write
+psev	LDR r12, =0x7
+	B write
+pD	LDR r12, =0x44
+	B write
+phash	LDR r12, =0x23
+	B write
+pzero	LDR r12, =0x0
+	B write
+pstar	LDR r12, =0x2A
+	B write	
 write	LDR r0, = USART2_BASE
 		LDR r1, [r0, #USART_ISR]
 		AND r1, r1, #USART_ISR_TXE
@@ -354,101 +236,7 @@ write	LDR r0, = USART2_BASE
 		BNE write
 setTDR	LDR r0, = USART2_BASE
 		STR r12, [r0, #USART_TDR]
-		B start
-
-
-pressedA
-	LDR r12, =0x41
-;	BL write
-	B write
-
-pressed3
-	LDR r12, =0x3
-
-	B write
-	
-pressed2 
-	LDR r12, =0x2
-
-	B write
-	
-pressed1
-	LDR r12, =0x1
-
-	B write
-	
-
-pressed6
-	LDR r12, =0x6
-
-	B write
-
-pressed5 
-	LDR r12, =0x5
-
-	B write
-
-pressed4	
-	LDR r12, =0x4
-
-	B write
-
-pressedC
-	LDR r12, =0x43
-
-	B write
-	
-pressed9
-	LDR r12, =0x9
-
-	B write
-
-pressed8 
-	LDR r12, =0x8
-
-	B write
-
-pressed7
-	LDR r12, =0x7
-
-	B write
-
-pressedD
-	LDR r12, =0x44
-
-	B write
-
-pressedhash
-	LDR r12, =0x23
-
-	B write
-
-pressed0
-	LDR r12, =0x0
-
-	B write
-
-pressedstar
-	LDR r12, =0x2A
-	
-	B write
-	
-
-	
-		
-	
-	
-	
-	
-	
-stop 	B 		stop     		; dead loop & program hangs here
-
+		B st
 	ENDP
-					
-	ALIGN			
 
-	AREA    myData, DATA, READWRITE
-	ALIGN
-;array	DCD   1, 2, 3, 4
-;mask    DCD   0x3F,0x6,0x1B,0x,0x,0x,0x,0x,0x ;array of masks for port E
 	END
